c7e658b5ede7f01258e1b60d271ca90b51675c88
William Sousa <williamrsousa94@hotmail.com>
2026-04-14 22:47:09 -0300

    DELETE Method

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const users = document.querySelector("#user-list");
-const appURL = "https://crudcrud.com/api/3322009ad3db46a7a0d3b2343a38a3fd";
+const appURL = "https://crudcrud.com/api/c091b19d38574f6ea6ab801079591acf";
 
 fetch(`${appURL}/users`)
   .then((response) => response.json())
   .then((userList) => {
     userList.forEach((user) => {
       const item = document.createElement("li");
+      item.id = `user-${user._id}`;
 
-      item.innerHTML = `<b>Nome: </b> ${user.name} <b>E-mail: </b> ${user.email} <button onclick(remove(user._id)) class="delete">X</button>`;
+      item.innerHTML = `<b>Nome: </b> ${user.name} <b>E-mail: </b> ${user.email} <button onclick="remove('${user._id}')" class="delete">X</button>`;
 
       users.appendChild(item);
+      console.log(user);
     });
   });
 
@@ -31,9 +33,32 @@ document.querySelector("#register").addEventListener("click", () => {
     .then((response) => response.json())
     .then((user) => {
       const item = document.createElement("li");
+      item.id = `user-${user._id}`;
 
-      item.innerHTML = `<b>Nome: </b> ${user.name} <b>E-mail: </b> ${user.email} <button onclick(remove(user._id)) class="delete">X</button>`;
+      item.innerHTML = `<b>Nome: </b> ${user.name} <b>E-mail: </b> ${user.email} <button onclick="remove('${user._id}')" class="delete">X</button>`;
 
       users.appendChild(item);
     });
 });
+
+function remove(id) {
+  fetch(`${appURL}/users/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: {
+      _id: id,
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao remover usuário do programa.");
+      }
+
+      return response.status === 204 ? {} : response.json().catch(() => ({}));
+    })
+    .then(() => {
+      document.querySelector(`#user-${id}`).remove();
+    });
+}
